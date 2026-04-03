e0fca82ee5cd21367aaec68fe05788d2a8ed69c6
RajakumarSDE <rajakumarsde4@gmail.com>
2026-04-03 14:34:37 +0530

    feat : Day 02 python programe

---
+++ day02.py
@@ -0,0 +1,28 @@
+# This is my day 2 python code 
+#print("Hello, World!")
+
+#a = 10
+#print (type(a))
+
+#raja = 100
+#print (raja)
+#print (type(raja))
+"""
+a = int("10")
+
+b = int("77")
+c = a+b
+"""
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+pre = num1 % num2
+print ("The addition of two number is : " , add)
+print ("The subtraction of two number is : " , sub)
+print ("The multiplication of two number is : " , mul)
+print ("The division of two number is : " , div)
+print ("The remainder of two number is : " , pre)
+
