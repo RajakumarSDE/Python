9f10488ed3bda10a66b6aae451021c0708704c53
RajakumarSDE <rajakumarsde4@gmail.com>
2026-04-08 17:27:53 +0530

    feat : added a day 5 code file

--- day05.py
+++ day05.py
@@ -1,4 +1,14 @@
+# here i have used if and else statement to check the condition and print the output
+
 if 100 < 1000:
     print("yes 100 is less than 1000. so this condition is true")
 else:
-    print("no 100 is not less than 1000. so this condition is false")    
+    print("no 100 is not less than 1000. so this condition is false")
+
+
+# here i have find something new using (ctrl + .) i could use all the emojis
+
+print("Hello Raja SDE 😍👋")
+
+# here also i find something new if i want to print something multiple times i can use the multiplication operator.
+print("Raja Kumar SDE 1" * 10)
