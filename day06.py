5e6c2fca0e7e4db48e4266004f9012898c022aab
RajakumarSDE <rajakumarsde4@gmail.com>
2026-04-09 17:25:48 +0530

    feat : added here some new lines of code

--- day06.py
+++ day06.py
@@ -9,12 +9,17 @@ print(rating)
 print(is_published)
 print(course_name)
 
-"""
+
 
 course_name = "python for beginners"
 print(len(course_name))
 print(course_name[3])
 print(course_name[-1]) 
 print(course_name[0:6])
-
+"""
 # i wached a video lucure about the 43:21
+
+Hello = "Python forthe \n beginners \"this\" is a new one "
+print(Hello)
+
+
