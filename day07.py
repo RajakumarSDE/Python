dce5ea96e8196cff227ff70a22d9c7b25dac726d
RajakumarSDE <rajakumarsde4@gmail.com>
2026-04-15 18:59:22 +0530

    day 07

---
+++ day07.py
@@ -0,0 +1,11 @@
+"""First = "Raja"
+Last = "Kumar"
+Full = f"{First} {Last}"
+print(Full)
+"""
+
+name = "Raja Kumar"
+print(name .upper())
+print(name .lower())
+
+# here i learned how to print a output  a whole upper case output an a lower case output
