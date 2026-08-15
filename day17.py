9f08b5a533531502f051b8a8fd18b597778bffca
RajakumarSDE <rajakumarsde4@gmail.com>
2026-08-15 20:05:35 +0530

    15.08.26 update by Raja SDE

---
+++ day17.py
@@ -0,0 +1,23 @@
+#good_vibe = 100
+
+#if happy_mood :
+  #  good_vibe = good_vibe + 100
+  #  my_feeling = "i am happy"
+
+#else :
+#    good_vibe = good_vibe - 100
+ #   my_feeling = "i am sad now"
+
+#print (good_vibe)
+#print (my_feeling)
+
+
+transaction_done = False
+
+if transaction_done :
+    print ("Transaction completed successfully.")
+    designation = input ("Enter your Designation : ")
+    print ("Your Designation is : " + designation)
+
+else :
+    print ("Transaction failed. Please try again.")
---
+++ day18.py
@@ -0,0 +1,10 @@
+money = 0 
+item = "greenrupee"
+
+if item == "greenrupee" :
+    money = money + 1
+
+else :
+    print("try sell this item to get money")
+
+    print ("money in pocket : " + str (money))    
